da429b94d2a32d101328df93c9b6d25938e2b6bf
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-07 10:41:57 +0100

    added edge case with multiple 100s

--- sort.test.js
+++ sort.test.js
@@ -10,5 +10,8 @@ describe('sort', () => {
   it('returns 0 when given 0', () => {
     expect(sort([0])).toEqual([0]);
   });
+  it('even returns when given 100 as part of array', ()=>{
+    expect(sort([5,10,10,100,100,55])).toEqual([100,55,10,5]);
+  });
   
 });
