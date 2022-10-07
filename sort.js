78627c98d7100704905a84147e2fe744417559c3
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-07 10:27:41 +0100

    tested with 0

--- sort.js
+++ sort.js
@@ -1,6 +1,9 @@
 const sort = (numbersArr) => {
+  // first sort the numbers into decending order
   let sortedArr = numbersArr.sort((a, b) => b - a);
+  // second remove the duplicates
   let uniqueArr = [...new Set(sortedArr)];
+  // third return the desired array. 
   return uniqueArr;
 }
 
--- sort.test.js
+++ sort.test.js
@@ -7,5 +7,8 @@ describe('sort', () => {
   it('returns and array in decending order with duplicates removed', () => {
     expect(sort([1,2,2,3])).toEqual([3,2,1]);
   });
+  it('returns 0 when given 0', () => {
+    expect(sort([0])).toEqual([0]);
+  })
 
 });
