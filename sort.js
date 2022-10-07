86cd4dbcda32e2651a64eba83d7ec19eead78d7a
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-07 10:22:17 +0100

    added second test and ran red

---
+++ sort.js
@@ -0,0 +1,5 @@
+const sort = (numbersArr) => {
+  return numbersArr.sort((a, b) => b - a);
+}
+
+module.exports= sort;
--- sort.test.js
+++ sort.test.js
@@ -4,4 +4,7 @@ describe('sort', () => {
   it('returns an array in decending order', () => {
     expect(sort([1,2,3])).toEqual([3,2,1]);
   });
+  it('returns and array in decending order with duplicates removed', () => {
+    expect(sort([1,2,2,3])).toEqual([3,2,1]);
+  });
 });
